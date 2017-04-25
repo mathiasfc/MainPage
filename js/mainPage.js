f973bcdbe411786995453279c190634857dbc062
Mathias Falci de Castro <mathias.falci@gmail.com>
2017-04-25 13:09:08 -0300

    new Header

--- js/mainPage.js
+++ js/mainPage.js
@@ -6,53 +6,35 @@
   
 //}, 3500);
 
+$( window ).resize(function() {
+  //var position = $("#myPerson").offset();
+  //console.log(position.top,position.left);
+});
+
 
 $(document).ready(function(){
 	$("#page").show();
-	document.body.appendChild(createLine(100, 100, 200, 200));
+	//var position = $("#myPerson").offset();
+	//y x width angle
+	//document.body.appendChild(createLineElement(37, 60, 120, 100));
+	//document.body.appendChild(createLineElement(352, 120, 200, 100));
 	//$(".loadingHourGlass").show();
-});
+	var target = $('#fixed-navbar').offset().top;
 
-$(function(){
-  $(window).scroll(function(){
-    var winTop = $(window).scrollTop();
-    if(winTop >= 30){
-      $("body").addClass("sticky-header");
-    }else{
-      $("body").removeClass("sticky-header");
+	$(window).scroll(function() {
+    
+    if ($(window).scrollTop() > target) {
+        
+        $('#fixed-navbar').addClass('sticky');
+        
+    } else {
+        
+        $('#fixed-navbar').removeClass('sticky');       
     }
-  });
+    
+});
+
 });
 
 
-function createLineElement(x, y, length, angle) {
-    var line = document.createElement("div");
-    var styles = 'border: 1px solid black; '
-               + 'width: ' + length + 'px; '
-               + 'height: 0px; '
-               + '-moz-transform: rotate(' + angle + 'rad); '
-               + '-webkit-transform: rotate(' + angle + 'rad); '
-               + '-o-transform: rotate(' + angle + 'rad); '  
-               + '-ms-transform: rotate(' + angle + 'rad); '  
-               + 'position: absolute; '
-               + 'top: ' + y + 'px; '
-               + 'left: ' + x + 'px; ';
-    line.setAttribute('style', styles);  
-    return line;
-}
 
-function createLine(x1, y1, x2, y2) {
-    var a = x1 - x2,
-        b = y1 - y2,
-        c = Math.sqrt(a * a + b * b);
-
-    var sx = (x1 + x2) / 2,
-        sy = (y1 + y2) / 2;
-
-    var x = sx - c / 2,
-        y = sy;
-
-    var alpha = Math.PI - Math.atan2(-b, a);
-
-    return createLineElement(x, y, c, alpha);
-}
